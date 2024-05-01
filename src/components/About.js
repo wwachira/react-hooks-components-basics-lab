a26508d38a4d4f95052798a49e6d255ce704e885
lizwachira <lizwanguwachira@gmail.com>
2024-05-01 21:44:30 +0300

    created 3components

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+function About() {
+    return (
+      <div id="about">
+        <h2>About</h2>
+      </div>
+    );
+  };
+
+  export default About
--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,15 @@
 import React from "react";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
-
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
-
-{/* write an <About> component here */}
-
-
+ 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
